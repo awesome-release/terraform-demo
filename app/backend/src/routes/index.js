ea7abb9ba719f3d91534b30c7e95ac983d630fb8
Michael Nunes <michael@releasehub.com>
2021-10-13 15:32:27 -0700

    Add slack integration

--- app/backend/src/routes/index.js
+++ app/backend/src/routes/index.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const slackBot = require("../client");
 
 const { validatePost } = require("../middleware/validate-post");
 
@@ -7,7 +8,24 @@ router.get("/", (req, res) => {
 });
 
 router.post("/new-post", validatePost, async (req, res) => {
-	res.sendStatus(201);
+	if (Number(process.env.SLACK_ACTIVE)) {
+		const { message } = req.query;
+
+		try {
+			await slackBot.chat.postMessage({
+				token: process.env.SLACK_TOKEN,
+				channel: process.env.SLACK_CHANNEL_ID,
+				text: `${message}`
+			});
+
+			res.sendStatus(201);
+		} catch (err) {
+			console.error(err.stack);
+			res.status(500).send(err);
+		}
+	} else {
+		res.sendStatus(201);
+	}
 });
 
 router.use((err, req, res, next) => {
